e2840c3fc2a3e7b66cce56dae2130f5201952729
johngiangrossi <john.giangrossi@email.com>
2025-07-04 21:40:05 +0200

    ADD ex-7

---
+++ src/App7.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class App7 {
+    public static void main(String[] args) {
+        /*
+        Snack8
+        Chiedi un numero di 4 cifre all’utente
+        e calcola la somma di tutte le cifre che compongono il numero.
+        */
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("inserire il primo numero intero di 4 cifre ");
+
+        int userNumber = scan.nextInt();
+        //System.out.println("il primo numero inserito è " + userNumber);
+
+    }
+}
