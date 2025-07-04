230f7b9be12dc7ecbdb28f7c55292edb47a70663
johngiangrossi <john.giangrossi@email.com>
2025-07-04 20:58:42 +0200

    first commit

---
+++ src/App.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        /*
+        Snack1
+        L’utente inserisce due numeri in successione.
+        Il software stampa il maggiore.
+        */
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("inserire il primo numero ");
+
+        int firstNumber = scan.nextInt();
+        System.out.println("il primo numero inserito è " + firstNumber);
+
+        System.out.print("inserire il secondo numero ");
+
+        int secondNumber = scan.nextInt();
+        System.out.println("il secondo numero inserito è " + secondNumber);
+
+        if (firstNumber > secondNumber) {
+            System.out.println("il numero " + firstNumber + " è maggiore");
+        } else if (secondNumber > firstNumber) {
+            System.out.println("il numero " + secondNumber + " è maggiore");
+        }
+    }
+}
